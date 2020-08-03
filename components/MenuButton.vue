a6b33ff51cf19000c36b8d8013d5350e1d025258
hanlee <me@hanlee.co>
2020-08-03 16:44:58 +0900

    Add NavBar

---
+++ components/MenuButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="menu-btn" :class="{ open: open }" @click="open = !open">
+    <div class="menu-btn__burger" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuButton',
+  data() {
+    return {
+      open: false,
+    }
+  },
+}
+</script>
+
+<style>
+.menu-btn {
+  @apply relative flex justify-center items-center w-8 h-8 cursor-pointer transition-all duration-500 ease-in-out;
+}
+
+.menu-btn__burger {
+  @apply w-8 h-1 bg-white rounded-full transition duration-500 ease-in-out;
+}
+.menu-btn__burger::before,
+.menu-btn__burger::after {
+  content: '';
+  @apply absolute w-8 h-1 bg-white rounded-full transition duration-500 ease-in-out;
+}
+.menu-btn__burger::before {
+  @apply transform -translate-y-3;
+}
+.menu-btn__burger::after {
+  @apply transform translate-y-3;
+}
+
+.menu-btn.open .menu-btn__burger {
+  @apply bg-transparent transform -translate-x-6;
+}
+.menu-btn.open .menu-btn__burger::before {
+  @apply transform rotate-45 translate-x-6;
+}
+.menu-btn.open .menu-btn__burger::after {
+  @apply transform -rotate-45 translate-x-6;
+}
+</style>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,25 +1,134 @@
 <template>
-  <header>
-    <div class="container navbar flex items-center justify-between mx-auto p-2">
-      <nuxt-link to="/">
-        <h1 class="text-xl lg:text-4xl font-semibold">ハンのブログ</h1>
-      </nuxt-link>
-      <div class="flex flex-wrap">
-        <nuxt-link to="/" class="px-3">關於我</nuxt-link>
-        <nuxt-link to="/" class="px-3">作品集</nuxt-link>
-        <nuxt-link to="/" class="px-3">Blog</nuxt-link>
-        <nuxt-link to="/" class="px-3">Github</nuxt-link>
+  <nav
+    class="flex items-center justify-between flex-wrap bg-teal-500 p-5 lg:px-10"
+  >
+    <nuxt-link to="/" class="flex items-center flex-shrink-0 text-white mr-6">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        aria-hidden="true"
+        focusable="false"
+        width="1.5em"
+        height="1.5em"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M18 5V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v1a3 3 0 0 0-3 3v11a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3zM8 4h8v4H8V4zm11 15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7a1 1 0 0 1 1 1z"
+        />
+      </svg>
+      <span class="font-semibold text-xl tracking-tight ml-2">ハンの Blog</span>
+    </nuxt-link>
+    <div class="block lg:hidden">
+      <MenuButton @click="toggle()" />
+    </div>
+    <div class="hidden lg:flex flex-grow w-full lg:items-center lg:w-auto">
+      <div class="flex-grow text-sm ml-6">
+        <nuxt-link
+          to="/"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white text-lg mr-6"
+        >
+          Home
+        </nuxt-link>
+        <nuxt-link
+          to="/"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white text-lg mr-6"
+        >
+          Blog
+        </nuxt-link>
+      </div>
+      <div>
+        <a
+          href="#"
+          class="inline-block text-sm px-3 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+        >
+          <div class="flex items-center flex-shrink-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              aria-hidden="true"
+              focusable="false"
+              width="1em"
+              height="1em"
+              class="mr-1"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2z"
+                fill="currentColor"
+              />
+            </svg>
+            <span>Github</span>
+          </div>
+        </a>
       </div>
     </div>
-  </header>
+    <div
+      v-show="show"
+      class="w-full block flex-grow lg:flex lg:items-center lg:w-auto mt-3"
+    >
+      <div class="text-sm lg:flex-grow">
+        <a
+          href="#responsive-header"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        >
+          Home
+        </a>
+        <a
+          href="#responsive-header"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        >
+          Blog
+        </a>
+      </div>
+      <div>
+        <a
+          href="#"
+          class="inline-block text-sm px-3 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+        >
+          <div class="flex items-center flex-shrink-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              aria-hidden="true"
+              focusable="false"
+              width="1em"
+              height="1em"
+              class="mr-1"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2z"
+                fill="currentColor"
+              />
+            </svg>
+            <span>Github</span>
+          </div>
+        </a>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-export default {}
+import { mapMutations } from 'vuex'
+import MenuButton from './MenuButton.vue'
+export default {
+  name: 'NavBar',
+  components: {
+    MenuButton,
+  },
+  computed: {
+    show() {
+      return this.$store.state.showNavBar
+    },
+  },
+  methods: {
+    ...mapMutations({ toggle: 'toggleNavBar' }),
+  },
+}
 </script>
 
-<style>
-.navbar {
-  height: 80px;
-}
-</style>
+<style></style>
