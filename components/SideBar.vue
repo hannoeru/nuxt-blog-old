3950ddbb4c3c8be128dcf66aaf622a2822058628
hanlee <me@hanlee.co>
2020-08-03 16:43:48 +0900

    Add SideBar

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="flex flex-col">
-    <SideBarItem v-for="n in 10" :key="n" />
-  </div>
+  <aside
+    class="flex flex-col w-full rounded-lg bg-white shadow-lg overflow-hidden mt-4 px-6 py-4"
+  >
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+      検索
+    </label>
+    <input
+      class="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      type="text"
+      placeholder="Search"
+    />
+  </aside>
 </template>
 
 <script>
-import SideBarItem from './SideBarItem'
 export default {
-  components: { SideBarItem },
+  name: 'SideBar',
 }
 </script>
 
